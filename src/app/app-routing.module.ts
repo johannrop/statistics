3bfb24f647904c6239541180d3ccc5c259c0b5ad
johann <johann.ortiz@unipamplona.edu.co>
2023-07-29 10:11:39 -0500

    finished login interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { ElementRef, NgModule, ViewChild } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'app-login',pathMatch:'full'},
+  {path:'app-login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  correo: string = '';
+  pass: string = '';
+  @ViewChild('correoInput') correoInput!: ElementRef;
+  @ViewChild('passInput') passInput!: ElementRef;
+ }
